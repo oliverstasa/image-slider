4a828b927b5a20c9a4071bfb919f64216481ce06
Oliver Staša <oliver.stasa@gmail.com>
2023-10-31 02:01:56 +0100

    Update index.js

--- demo/index.js
+++ demo/index.js
@@ -1,24 +1,31 @@
 let lastPos;
 let elem;
+
+const getCursorPos = e => e.pageY || e.touches[0].pageY;
 const clamp = (value, min, max) => Math.min(Math.max(value, min), max);
+
 const setRange = e => {
     if (!elem) {
         return;
     }
+    // could be reduced to elem.offsetHeight + lastPos - e.pageY
     const newValue = clamp(elem.getAttribute('value')*1 + (lastPos - e.pageY), 0, 100);
-    lastPos = e.pageY || e.touches[0].pageY;
+    lastPos = getCursorPos(e);
     elem.setAttribute('value', newValue);
 };
+
 const hook = e => {
     elem = e.target;
-    lastPos = e.pageY || e.touches[0].pageY;
+    lastPos = getCursorPos(e);
     document.addEventListener('mousemove', setRange);
     document.addEventListener('mouseup', unhook);
 };
+
 const unhook = _ => {
     document.removeEventListener('mousemove', setRange);
     document.removeEventListener('mouseup', unhook);
 };
+
 window.addEventListener('load', _ => {
     const images = document.querySelectorAll('div');
     for (const image of images) {
